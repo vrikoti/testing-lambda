bffb983724468ca791c85998c0d423b193fe78fa
anudeep <anvarikoti@gmail.com>
2024-10-22 00:16:13 +0530

    this should triggers

--- test-1/test-1.py
+++ test-1/test-1.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     print("Event:", event)
     
     # Example logic for testing
-    message = "working ahhhhh?!!"
+    message = "working ccccccccccahhhhh?!!"
     
     # Example response
     response = {
