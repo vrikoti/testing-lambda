1a6d423482a748b5e76fa770fee19ef9b71c7304
anudeep <anvarikoti@gmail.com>
2024-10-22 00:44:37 +0530

    ok

--- test-1/test-1.py
+++ test-1/test-1.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     print("Event:", event)
     
     # Example logic for testing
-    message = "workng thanks now, pls work!"
+    message = "workng thanks now, pls work, gn bye!"
     
     # Example response
     response = {
