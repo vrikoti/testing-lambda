b25b6be91cf37a9e4895ddc8ebbfe8ef8b98d4dc
avarikoti <139532383+avarikoti@users.noreply.github.com>
2024-10-22 13:14:45 +0530

    Update test-3.py

--- test-3/test-3.py
+++ test-3/test-3.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     print("Event:", event)
     
     # Example logic for testing
-    message = "Helloooooooooooo!"
+    message = "Helloooooooooooowww!"
     
     # Example response
     response = {
